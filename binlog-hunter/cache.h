4f66246e9ac347d9d2b53357cfa89652aa9f2d35
smallwalk <2232445050@qq.com>
2015-05-26 18:18:18 +0800

    cache

--- binlog-hunter/cache.h
+++ binlog-hunter/cache.h
@@ -5,6 +5,7 @@
 class cache_key_factory{
 	public:
 		int load(config& conf);
+		cache_key* table2key(std::string& db, std::string& table, mysql::Row_of_fields& fields);
 
 	private:
 		std::map<std::string, cache_key> m_key_map;
